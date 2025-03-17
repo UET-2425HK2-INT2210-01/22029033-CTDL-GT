ba3b3101f7ef3df17d34d68481ed51f5077a8e9d
22029033 <22029033@vnu.edu.vn>
2025-03-17 14:50:13 +0700

    Add files via upload

---
+++ 22029033_Lect4_Assignments/Task 1.cpp
@@ -0,0 +1,7 @@
+Hai tính chất mong muốn của một hàm băm là:
+
+1. Tính xác định (Determinism): 
+Cùng một đầu vào luôn tạo ra cùng một đầu ra. Điều này đảm bảo tính nhất quán khi lưu trữ và truy xuất dữ liệu.
+
+2. Tính phân bố đều (Uniformity - Giảm va chạm): 
+Hàm băm tốt nên phân phối các đầu vào đồng đều trên không gian đầu ra, giúp giảm thiểu khả năng hai giá trị khác nhau tạo ra cùng một giá trị băm (va chạm).
